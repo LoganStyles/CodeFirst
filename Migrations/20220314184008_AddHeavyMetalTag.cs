f63ce218ccf755546c767b3f5eb7f49de5a44849
LoganStyles <emmaist23@gmail.com>
2022-03-14 19:50:41 +0100

    reverted the migration that added heavy metal to the Tags table

---
+++ Migrations/20220314184008_AddHeavyMetalTag.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirst.Migrations
+{
+    public partial class AddHeavyMetalTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Title" },
+                values: new object[] { 6L, "Heavy Metal" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 6L);
+        }
+    }
+}
