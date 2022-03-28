313c8a557980668b48f21abe1238df23b7c64d2b
LoganStyles <emmaist23@gmail.com>
2022-03-28 15:19:50 +0100

    feat(client vs server evaluation): illustrating top level projection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Code first!");
+//Console.WriteLine("Hello, LINQ!");
+
+using CodeFirst.Data;
+
+namespace CodeFirstTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ArtistsContext();
+            var employees = context.Employees
+                .Select(
+                    employee =>
+                        new
+                        {
+                            Id = employee.Id,
+                            Name = FormatNames(employee.FirstName, employee.LastName)
+                        }
+                )
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0} {1}", employee.Id, employee.Name);
+            }
+            
+        }
+
+        public static string FormatNames(string firstname, string lastname)
+        {
+            var result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(firstname) && !string.IsNullOrWhiteSpace(lastname))
+            {
+                result = firstname.ToUpper() + ' ' + lastname.ToUpper();
+            }
+
+            return result;
+        }
+    }
+}
