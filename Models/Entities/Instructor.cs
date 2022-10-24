b3acf3740c56dd068fe00b1a9737c201ede95a95
LoganStyles <emmaist23@gmail.com>
2022-10-24 16:28:05 +0100

    feat(migrations - challenge): added properties - deptid & salary to Instructor entity

--- Models/Entities/Instructor.cs
+++ Models/Entities/Instructor.cs
@@ -7,6 +7,8 @@ namespace CodeFirst.Models.Entities
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public long DeptId { get; set; }
+        public decimal Salary { get; set; }
 
         public Course Course { get; set; }
     }
