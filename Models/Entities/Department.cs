a88ac6c36dd55ed3008a86bd32b16c0e59267e4e
LoganStyles <emmaist23@gmail.com>
2022-12-14 10:20:48 +0100

    feat(department): rename title and delete HOD properties

--- Models/Entities/Department.cs
+++ Models/Entities/Department.cs
@@ -3,7 +3,14 @@ namespace CodeFirst.Models.Entities{
     public class Department{
 
         public long Id { get; set; }
-        public string Title { get; set; }
-        public string HOD { get; set; }
+
+        /* Rename the `Title` property of the `Department` entity to `Name` */
+
+        // public string Title { get; set; } 
+        public string Name { get; set; }
+
+        /* Delete the `HOD` property of the `Department` entity */
+
+        //public string HOD { get; set; }
     }
 }
